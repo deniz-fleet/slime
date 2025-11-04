c5006d38d320211c329fb25d8c545d692ce210fd
Deniz <deniz@Denizs-MacBook-Pro.local>
2025-11-04 12:10:42 -0800

    fix

--- tools/convert_hf_to_torch_dist.py
+++ tools/convert_hf_to_torch_dist.py
@@ -93,10 +93,9 @@ def main():
     bridge_class_name = bridge.__class__.__name__
     if bridge_class_name in ("Qwen2_5VLBridge", "Qwen3VLBridge"):
         model = bridge.get_model(weight_path=hf_model_path)
-        bridge.load_weights(model, hf_model_path, memory_efficient=True)
     else:
         model = get_model(get_model_provider_func(args), ModelType.encoder_or_decoder, wrap_with_ddp=False)
-        bridge.load_weights(model, hf_model_path, memory_efficient=True)
+    bridge.load_weights(model, hf_model_path, memory_efficient=True)
     print(f"Model loaded: {hf_model_path=}, {model=}")
 
     save_checkpoint(1, model, None, None, 0)

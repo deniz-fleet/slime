b39d32127769ff3d55c07805ef8850b80e5ef3d5
Deniz <deniz@Denizs-MBP.localdomain>
2025-11-05 17:45:20 -0800

    lets go

--- scripts/run-qwen2.5-vl-7B-fleet.sh
+++ scripts/run-qwen2.5-vl-7B-fleet.sh
@@ -42,7 +42,7 @@ ROLLOUT_ARGS=(
   --n-samples-per-prompt 2
   --global-batch-size 8
   --num-rollout 50
-  --rollout-max-response-len 131072
+  --rollout-max-response-len 16384
 )
 DEBUG_ARGS=(
    --save-debug-rollout-data /workspace/rollouts/{wandb_run_id}/

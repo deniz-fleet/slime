e8fd62a54383a7c02deaef2f5d0a63d305cb3ee9
Deniz <deniz@Denizs-MacBook-Pro.local>
2025-11-06 09:43:48 -0800

    uncomment

--- examples/fleet-env/run_fleet_qwen2_5_vl_7b_rl.sh
+++ examples/fleet-env/run_fleet_qwen2_5_vl_7b_rl.sh
@@ -18,13 +18,13 @@ SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 SLIME_ROOT="$(cd "${SCRIPT_DIR}/../../" && pwd)"
 source "${SLIME_ROOT}/scripts/models/qwen2.5-7B.sh"
 
-# PYTHONPATH=/root/Megatron-LM python tools/convert_hf_to_torch_dist.py \
-#   ${MODEL_ARGS[@]} \
-#   --hf-checkpoint /root/Qwen2.5-VL-7B-Instruct \
-#   --save /root/Qwen2.5-VL-7B_torch_dist \
-#   --ckpt-format torch_dcp \
-#   --use-torch-fsdp2 \
-#   --no-gradient-accumulation-fusion
+PYTHONPATH=/root/Megatron-LM python tools/convert_hf_to_torch_dist.py \
+  ${MODEL_ARGS[@]} \
+  --hf-checkpoint /root/Qwen2.5-VL-7B-Instruct \
+  --save /root/Qwen2.5-VL-7B_torch_dist \
+  --ckpt-format torch_dcp \
+  --use-torch-fsdp2 \
+  --no-gradient-accumulation-fusion
 
 # 3) Run RL with separate train/rollout GPUs
 bash scripts/run-qwen2.5-vl-7B-fleet.sh

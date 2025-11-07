d9a502a6a8fe4fb48280c5722cb7aefdbdc1f572
Deniz <deniz@Denizs-MacBook-Pro.local>
2025-11-07 14:03:08 -0800

    set TP 4

--- scripts/run-qwen3-30B-A3B-fleet.sh
+++ scripts/run-qwen3-30B-A3B-fleet.sh
@@ -58,7 +58,7 @@ EVAL_ARGS=(
 )
 
 PERF_ARGS=(
-   --tensor-model-parallel-size 1
+   --tensor-model-parallel-size 4
    --sequence-parallel
    --context-parallel-size 1
    #--expert-model-parallel-size 8

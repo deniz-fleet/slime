598270732d99f61d16579b159fb1d5d3542eafbe
Deniz <deniz@Denizs-MacBook-Pro.local>
2025-11-07 15:58:02 -0800

    parallel

--- scripts/run-qwen3-30B-A3B-fleet.sh
+++ scripts/run-qwen3-30B-A3B-fleet.sh
@@ -55,11 +55,11 @@ DEBUG_ARGS=(
 
 
 PERF_ARGS=(
-   --tensor-model-parallel-size 4
+   --tensor-model-parallel-size 2
    --sequence-parallel
    --context-parallel-size 1
    #--expert-model-parallel-size 8
-   --expert-model-parallel-size 1
+   --expert-model-parallel-size 2
    --expert-tensor-parallel-size 1
 
    --recompute-granularity full

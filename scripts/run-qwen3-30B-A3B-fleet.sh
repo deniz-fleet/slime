62f6949e6a95830c3d915b2e948c2b967fbb9ce9
Deniz <deniz@Denizs-MacBook-Pro.local>
2025-11-07 18:50:57 -0800

    lets go

--- scripts/run-qwen3-30B-A3B-fleet.sh
+++ scripts/run-qwen3-30B-A3B-fleet.sh
@@ -56,10 +56,10 @@ DEBUG_ARGS=(
 
 
 PERF_ARGS=(
-   --tensor-model-parallel-size 1
+   --tensor-model-parallel-size 2
    #--sequence-parallel
    --context-parallel-size 1
-   --expert-model-parallel-size 8
+   --expert-model-parallel-size 4
    --expert-tensor-parallel-size 1
 
    --recompute-granularity full
@@ -67,7 +67,7 @@ PERF_ARGS=(
    --recompute-num-layers 4
 
    --use-dynamic-batch-size
-   --max-tokens-per-gpu 4096
+   --max-tokens-per-gpu 2048
 )
 
 GRPO_ARGS=(
